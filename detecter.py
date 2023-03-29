f85a739fcdd7a145b5232f1769d9e0d7f9f571f4
Mritunjay Pandey <121624941+Mritunjay13@users.noreply.github.com>
2023-03-29 23:20:04 +0530

    Add files via upload

--- detecter.py
+++ detecter.py
@@ -13,7 +13,7 @@ user_file = st.sidebar.file_uploader("Upload_file", type=['csv', 'xlsx', 'pdf'],
 if user_file is not None:
     data = pd.read_csv(user_file)
     df = processor.preprocess(data)
-    st.title('Loan Fraud Detection Analysis')
+    st.title(':red[Loan Fraud Detection Analysis]')
     v1 = st.title('Loan Data')
     v2 = st.dataframe(df)
     total_memeber = Analysis.totalmember(df)
@@ -47,7 +47,7 @@ if user_file is not None:
         plt.figure(figsize=(18, 25))
         # subplot 1: loan type
         plt.subplot(4, 2, 1)
-        my_plot = sns.countplot(x='purpose', data=fully_paid)
+        my_plot = sns.countplot(y='purpose', data=fully_paid)
         my_plot.set_xticklabels(my_plot.get_xticklabels(), rotation=90)
         plt.title("TYPES OF LOAN", size=20)
  
@@ -87,7 +87,7 @@ if user_file is not None:
     st.subheader("Total Members: {}".format(total_memeber))
 
     # univariate analysis on contract status Approved
-    st.header(':green[Univariate Analysis on current status  Charged Off ]')
+    st.header('Univariate Analysis on current status of Charged Off')
     tab1, tab2, tab3, tab4= st.tabs(["Pupose", "Term", "Grade", "Home_Ownership"])
 
     with tab1:
@@ -95,10 +95,9 @@ if user_file is not None:
         plt.figure(figsize=(18, 25))
         # subplot 1: loan type
         plt.subplot(4, 2, 1)
-        my_plot = sns.countplot(x='purpose', palette='gist_heat_r', data=charge_off)
+        my_plot = sns.countplot(y='purpose',data=charge_off)
         my_plot.set_xticklabels(my_plot.get_xticklabels(), rotation=90)
         plt.title("TYPES OF LOAN", size=20)
-        st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
 
     with tab2:
@@ -106,9 +105,8 @@ if user_file is not None:
         # subplot 2: Term
         plt.figure(figsize=(18, 25))
         plt.subplot(4, 2, 2)
-        sns.countplot(x='term', palette='gist_heat_r', data=charge_off)
+        sns.countplot(x='term', data=charge_off)
         plt.title("Term of Months", size=20)
-        st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
 
     with tab3:
@@ -117,26 +115,24 @@ if user_file is not None:
         plt.subplot(4, 2, 3)
         sns.countplot(x='grade', palette='gist_heat_r', data=charge_off)
         plt.title("Grade of User", size=20)
-        st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
     with tab4:
         plt.figure(figsize=(18, 25))
         plt.subplot(4, 2, 4)
-        sns.countplot(x='home_ownership', palette='gist_heat_r', data=charge_off)
+        sns.countplot(x='home_ownership', data=charge_off)
         plt.title("Home Ownership", size=20)
-        st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
 
 
     # Display those member whose current status is Current
-    st.title(':blue[Current Status Members Details]')
+    st.title('Current Status Members Details')
     current = Analysis.current_u(df)
     st.dataframe(current)
     total_memeber = Analysis.totalmember(current)
     st.subheader("Total Members: {}".format(total_memeber))
 
     # univariate analysis on contract status Approved
-    st.header(':green[Univariate Analysis on current status  Current Memebr]')
+    st.header('Univariate Analysis on current status of current Members')
     tab1, tab2, tab3, tab4= st.tabs(["Pupose", "Term", "Grade", "Home_Ownership"])
 
     with tab1:
@@ -144,10 +140,9 @@ if user_file is not None:
         plt.figure(figsize=(18, 25))
         # subplot 1: loan type
         plt.subplot(4, 2, 1)
-        my_plot = sns.countplot(x='purpose', palette='gist_heat_r', data=current)
+        my_plot = sns.countplot(y='purpose', data=current)
         my_plot.set_xticklabels(my_plot.get_xticklabels(), rotation=90)
         plt.title("TYPES OF LOAN", size=20)
-        st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
 
     with tab2:
@@ -155,34 +150,35 @@ if user_file is not None:
         # subplot 2: Term
         plt.figure(figsize=(18, 25))
         plt.subplot(4, 2, 2)
-        sns.countplot(x='term', palette='gist_heat_r', data=current)
+        sns.countplot(x='term', data=current)
         plt.title("Term of Months", size=20)
-        st.set_option('deprecation.showPyplotGlobalUse', False)
+        
         st.pyplot()
 
     with tab3:
-        st.header("An owl")
+        st.header("Grade of user")
         plt.figure(figsize=(18, 25))
         plt.subplot(4, 2, 3)
-        sns.countplot(x='grade', palette='gist_heat_r', data=current)
+        sns.countplot(x='grade', data=current)
         plt.title("Grade of User", size=20)
         st.pyplot()
     with tab4:
+        st.header("Home Ownership")
         plt.figure(figsize=(18, 25))
         plt.subplot(4, 2, 4)
-        sns.countplot(x='home_ownership', palette='gist_heat_r', data=current)
+        sns.countplot(x='home_ownership', data=current)
         plt.title("Home Ownership", size=20)
 
         st.pyplot()
 
 
 
-    st.title('Bivariate Analysis on Type of loan and Total Payment of loan')
+    st.title('Type of loan and Total Payment')
     fig, ax = plt.subplots(figsize=(10, 5))
     ax = plt.gca()
-    ax.plot(df['purpose'], df['total_pymnt'], 'o', c='red', alpha=0.1)
+    ax.plot(df['total_pymnt'],df['purpose'], 'o', c='blue', alpha=0.1)
     ax.set_xlabel('Type of Loan')
-    ax.set_ylabel('Total amount of Pyment')
+    ax.set_ylabel('Total ammount of Payment')
     plt.xticks(rotation=90)
 
     st.pyplot(fig)
